e435c7f6495e232e6c51cc76b8304dd63da45ab0
Sophia <zhuks@wwu.edu>
2024-01-25 23:24:15 -0800

    reset transformaiton

--- docs/pages/assignments/transformation/script.js
+++ docs/pages/assignments/transformation/script.js
@@ -1,36 +1,63 @@
-// Get all the number elements
-const numbers = document.querySelectorAll('.number');
+$(function() {
 
-// Add click event listener to each number element
-numbers.forEach((number, index) => {
-    number.addEventListener('click', () => {
-        // Get the window element
-        const window = document.querySelector('.window');
+    $("#one, #two, #three, #four, #five, #six, #seven").on("click", function() {
+        $(".bird").show();
+        $(".bat").hide();
+    });
 
-        // Calculate the color value based on the index of the clicked number
-        const colorValue = Math.floor((index / (numbers.length - 1)) * 255); // Reverse the order
+    $("#eight, #nine, #ten, #eleven, #twelve").on("click", function() {
+        $(".bird").hide();
+        $(".bat").show();
+    });
 
-        // Set the background color of the window element based on the calculated color value
-        const minColor = [135, 206, 250]; // Light blue
-        const maxColor = [0, 0, 139]; // Dark blue
-        const r = minColor[0] + Math.floor((maxColor[0] - minColor[0]) * (colorValue / 255));
-        const g = minColor[1] + Math.floor((maxColor[1] - minColor[1]) * (colorValue / 255));
-        const b = minColor[2] + Math.floor((maxColor[2] - minColor[2]) * (colorValue / 255));
+    $("#one, #two, #three, #four, #five, #six, #seven, #eight, #nine, #ten, #eleven, #twelve").on("click", function() {
+        var id = $(this).attr("id");
+        var color;
 
-        // Add a smooth transition to the background color change
-        window.style.transition = 'background-color 0.5s ease-in-out';
-
-        // Set the background color of the window element based on the calculated color value
-        window.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-
-        // Show or hide the .window:after element based on the index of the clicked number
-        const windowAfter = document.querySelector('.window:after');
-        if () {
-            windowAfter.style.display = 'block';
-        } else {
-            windowAfter.style.display = 'none';
+        switch (id) {
+            case "one":
+                color = "#73ccff";
+                break;
+            case "two":
+                color = "#4cb7f5";
+                break;
+            case "three":
+                color = "#48a8f7";
+                break;
+            case "four":
+                color = "#3b98eb";
+                break;
+            case "five":
+                color = "#2a7fe8";
+                break;
+            case "six":
+                color = "#2376db";
+                break;
+            case "seven":
+                color = "#2376db";
+                break;
+            case "eight":
+                color = "#3b85d4";
+                break;
+            case "nine":
+                color = "#2d6fb5";
+                break;
+            case "ten":
+                color = "#2d6fb5";
+                break;
+            case "eleven":
+                color = "#20549e";
+                break;
+            case "twelve":
+                color = "#234596";
+                break;
+            default:
+                color = "#000000";
+                break;
         }
+
+        $(".window").animate({
+            backgroundColor: color,
+        }, 1000);
     });
 });
-
-
