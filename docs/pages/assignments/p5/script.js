cf37bb3d57c5ca2839faa3dd35251e8337fb8baa
Sophia <zhuks@wwu.edu>
2024-02-01 23:13:41 -0800

    added painting (no animation)

---
+++ docs/pages/assignments/p5/script.js
@@ -0,0 +1,45 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(240);
+    yellowTriangle();
+    redTriangle();
+    blackTriangle();
+    darkTriangle();
+    lightTriangle();
+  }
+  
+  function yellowTriangle() {
+    const y = color(255, 204, 0);
+    fill(y);
+    triangle(100, 300, 0, 400, 0, 200);
+  
+    noStroke();
+  }
+  function redTriangle() {
+    const r = color(245, 66, 66);
+    fill(r);
+    triangle(150, 250, 400, 0, 400, 500);
+    noStroke();
+  }
+  function blackTriangle() {
+    const b = color(46);
+    fill(b);
+    triangle(-50, 200, 150, -10, 150, 400);
+    noStroke();
+  }
+  function darkTriangle() {
+    const d = color(180);
+    fill(d);
+    triangle(400, 0, 300, 100, 200, 0);
+    noStroke();
+  }
+  function lightTriangle() {
+    const d = color(210);
+    fill(d);
+    triangle(150, 400, 150, 250, 300, 400);
+    noStroke();
+  }
+  
